b5efe758f596b197b232e22a48c4b3612570ec18
Stuart Auld <sauld@cozero.com.au>
2016-01-04 12:37:04 +1100

    update result method for WeeklyResults

--- app.rb
+++ app.rb
@@ -61,4 +61,15 @@ end
 class WeeklyResult < ActiveRecord::Base
   belongs_to :user
   belongs_to :week
+
+  def update_result
+    dry_days = 0
+    Date::DAYNAMES.map{|x| x.downcase}.each do |day|
+      dry_days += (self.send "#{day}_drinks") == 0 ? 1 : 0
+    end
+    self.dry_days = dry_days
+    self.score = 10 + ([dry_days - 3,0].min) * 5
+    self.save
+  end
+
 end
