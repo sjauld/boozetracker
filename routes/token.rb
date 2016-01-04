a0750d475541d9100c4a63d96c36310a583d5372
Stuart Auld <sauld@cozero.com.au>
2016-01-04 12:26:25 +1100

    use redirects with messages for token page

--- routes/token.rb
+++ routes/token.rb
@@ -3,19 +3,20 @@ class App < Sinatra::Base
   get '/token/:token' do
     packet = JSON.parse($redis.get(params[:token])) rescue nil
     if packet.nil?
-      'It looks like this token has expired. Maybe try updating your results via the <a href="/">website</a>.'
+      flash[:error] = 'It looks like this token has expired.'
     else
       my_result = WeeklyResult.find(packet['result'])
       my_result.send "#{packet['parameter']}=", params[:result] == 'yes' ? true : params[:result] == 'no' ? false : nil
       my_result.save
       if params[:result] == 'yes'
-        'Mmmm... beer.'
+        flash[:notice] = 'Mmmm... beer.'
       elsif params[:result] == 'no'
-        "Nice one. You have had #{my_result.dry_days.to_i} dry day(s) this week."
+        flash[:notice] = "Nice one. You have had #{my_result.dry_days.to_i} dry day(s) this week."
       else
-        'I think you stuffed something up there'
+        flash[:error] = 'I think you stuffed something up there'
       end
     end
+    redirect to('/')
   end
 
 end
