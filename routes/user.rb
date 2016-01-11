62321665e43917d08a7d4874493b2d304f11ca5c
Stuart Auld <sauld@cozero.com.au>
2016-01-11 21:42:51 +1100

    unsubscribe function

--- routes/user.rb
+++ routes/user.rb
@@ -4,6 +4,24 @@ class App < Sinatra::Base
     @this_user = User.find(params[:user]) rescue @user
     haml :profile
   end
-  
+
+  get '/unsubscribe' do
+    user = User.find(params[:user]) rescue @user
+    if user.nil?
+      flash[:error] = 'User ID incorrect or not provided'
+      redirect '/'
+    end
+    message = Mail.new do
+      from    ENV['EMAIL_FROM']
+      to      "sja@marsupialmusic.net"
+      subject 'Boozetracker user unsubscribed'
+      content_type 'text/html; charset=UTF-8'
+      body    "#{user.name} has clicked unsubscribe"
+      delivery_method Mail::Postmark, api_token: ENV['POSTMARK_API_TOKEN']
+    end
+    message.deliver
+    flash[:notice] = "You may have been unsubscribed"
+    redirect '/'
+  end
 
 end
